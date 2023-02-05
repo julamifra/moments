63ee5c92fbfc40fc857aa5ecf03d14e01bf94210
julamifra <julamifra@gmail.com>
2023-02-05 22:44:13 +0000

    route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import styles from './App.module.css'
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="styles.App">
       <NavBar />
       <Container className='styles.Main'>
+        <Switch>
+          <Route exact path="/" render={()=><h1>Home Page!</h1>}/>
+        </Switch>
+
         <h1>Home Page</h1>
         <h1>Sign in</h1>
       </Container>
