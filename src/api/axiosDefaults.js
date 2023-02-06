96703213489b023643cb373bdc7bb1b8dc0f3d1c
julamifra <julamifra@gmail.com>
2023-02-06 14:01:23 +0000

    signUpForm finish

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://julian-drf-api.herokuapp.com/'
-axios.defaults.headers.post['Content-type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://julian-drf-api.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
