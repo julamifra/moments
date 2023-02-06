9b4010ac08c0d100e8cf13b6ac089cf3e32ec085
julamifra <julamifra@gmail.com>
2023-02-06 00:58:51 +0000

    add signUpForm

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import styles from './App.module.css'
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
-    <div className="styles.App">
+    <div className={styles.App}>
       <NavBar />
-      <Container className='styles.Main'>
+      <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={()=><h1>Home Page!</h1>}/>
+          <Route exact path="/" render={()=><h1>Home Page</h1>}/>
+          <Route exact path="/signin" render={()=><h1>Sign in</h1>}/>
+          <Route exact path="/signup" render={()=><SignUpForm />}/>
+          <Route render={()=><h1>Page not found!</h1>}/>
         </Switch>
-
-        <h1>Home Page</h1>
-        <h1>Sign in</h1>
       </Container>
     </div>
   );
