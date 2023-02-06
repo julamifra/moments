4914d6a077169048c0e611f8af010c2d367de527
julamifra <julamifra@gmail.com>
2023-02-06 15:09:02 +0000

    create CurrentUserContext

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
 
 function App() {
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={()=><h1>Home Page</h1>}/>
-          <Route exact path="/signin" render={()=><h1>Sign in</h1>}/>
-          <Route exact path="/signup" render={()=><SignUpForm />}/>
-          <Route render={()=><h1>Page not found!</h1>}/>
-        </Switch>
-      </Container>
-    </div>
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={()=><h1>Home Page</h1>}/>
+              <Route exact path="/signin" render={()=><SignInForm />}/>
+              <Route exact path="/signup" render={()=><SignUpForm />}/>
+              <Route render={()=><h1>Page not found!</h1>}/>
+            </Switch>
+          </Container>
+        </div>
   );
 }
 
